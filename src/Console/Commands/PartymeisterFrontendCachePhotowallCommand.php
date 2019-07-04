4a959e9f8dc6cdcb2b970484f08401f0f5087922
Reza Esmaili <bitbucket@dfox.info>
2019-07-04 11:41:18 +0200

    Reformatting

--- src/Console/Commands/PartymeisterFrontendCachePhotowallCommand.php
+++ src/Console/Commands/PartymeisterFrontendCachePhotowallCommand.php
@@ -23,8 +23,10 @@ class PartymeisterFrontendCachePhotowallCommand extends Command
      */
     protected $description = 'Make symlinks to all uploaded files';
 
-    protected function mkdir($directory) {
-        if (!is_dir($directory)) {
+
+    protected function mkdir($directory)
+    {
+        if ( ! is_dir($directory)) {
             mkdir($directory);
         }
     }
@@ -37,28 +39,25 @@ class PartymeisterFrontendCachePhotowallCommand extends Command
      */
     public function handle()
     {
-        $basePath = base_path('public/photowall');
+        $basePath  = base_path('public/photowall');
         $cachePath = base_path('public/photowall/cache');
-        $source = '';
-        if (!is_dir($cachePath)) {
+        $source    = '';
+        if ( ! is_dir($cachePath)) {
             mkdir($cachePath);
         }
-        if (!is_dir($cachePath.'/'.$source)) {
-            mkdir($cachePath.'/'.$source);
+        if ( ! is_dir($cachePath . '/' . $source)) {
+            mkdir($cachePath . '/' . $source);
         }
-
 
         foreach (Storage::disk('photowall')->files($source) as $file) {
             $split = explode('/', $file);
-            $name = array_values(array_slice($split, -1))[0];
+            $name  = array_values(array_slice($split, -1))[0];
             //$target = str_replace($source, '2018', $);
-            if (!file_exists($cachePath.'/'.$name)) {
-                 try {
-                    Image::load($basePath.'/'.$file)
-                         ->width(1280)
-                         ->save($cachePath.'/'.$name);
+            if ( ! file_exists($cachePath . '/' . $name)) {
+                try {
+                    Image::load($basePath . '/' . $file)->width(1280)->save($cachePath . '/' . $name);
 
-                    $this->info($file. ' converted');
+                    $this->info($file . ' converted');
                 } catch (\Exception $e) {
                     $this->error($e->getMessage());
                 }
