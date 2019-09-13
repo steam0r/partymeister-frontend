5d07b37bca326281c8f3c5f581fe0d2d224d9ed0
Reza Esmaili <github@dfox.info>
2019-09-13 16:43:46 +0200

    Remove photowall route

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,1 @@
 <?php
-
-Route::group([
-    'as'         => 'frontend.',
-    'prefix'     => '',
-    'namespace'  => 'Partymeister\Frontend\Http\Controllers\Frontend',
-    'middleware' => [
-        'web',
-    ]
-], function () {
-    Route::get('photos', 'PhotowallController@index')->name('photowall');
-});
